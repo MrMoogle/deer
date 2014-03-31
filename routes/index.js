cd308319483b2d44744d7ef57dabb085788b61d7
Erick Chen <eychen@princeton.edu>
2014-03-30 21:18:40 -0400

    map

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Princeton Free Food Map' });
 };
+
+exports.map = function(req, res){
+  res.render('map', { title: 'Look at my map bitchez!' });
+};
