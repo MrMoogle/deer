1cf2356a6f21ac5c2c16fce917872d37b9e295ea
Oscar Li <nibbles913@gmail.com>
2014-04-08 02:05:28 -0400

    organizational stuff

--- test.js
+++
@@ -1,5 +0,0 @@
-var date = new Date();
-var curr_time = (date.getYear() + 1900) + '-' + date.getMonth() + '-' + date.getDate() + ' '
-                   + date.getHours() + ':' + date.getMinutes() + ":" + date.getSeconds();
-
-console.log(curr_time);
