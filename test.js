eeee842c296551cc1d8b10234bf3f7a11d5176a5
Oscar Li <nibbles913@gmail.com>
2014-04-10 11:37:54 -0400

    cleanup

--- test.js
+++
@@ -1,5 +0,0 @@
-var fs          = require('fs');
-var foodlist = fs.readFileSync('./public/text/food.txt').toString().split("\n");
-console.log(foodlist[0].charCodeAt(8));
-console.log("eggplant".charCodeAt(8));
-console.log(foodlist[0]);
